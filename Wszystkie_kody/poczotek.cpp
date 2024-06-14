d8bfa63e97e0d84391a2200fc7fb662b7b2a485d
WWozniakProgrammer <155389887+WWozniakProgrammer@users.noreply.github.com>
2024-06-14 22:57:52 +0200

    Pierwsze koty nieloty
    jest już późno, ale udało mi się zrobić program w Cpp który pobiera od użytkownika nazwę spółki i odpala scrapera w pythonie przekazując mu ową spółkę... Przy okazji program sam orientuje się gdzie mamy pythona, to sprytne rozwiązanie pozwala na lepszą integrację aplikacji ze środowiskiem

---
+++ Wszystkie_kody/poczotek.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <array>
+#include <memory>
+
+using namespace std;
+// ta funkcja zwraca ścieżkę z pythonem (nawet jak ma się ich kilka to zwraca tą która działa ;)
+string get_first_python_path() {
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen("where python", "r"), pclose);
+    if (!pipe) {
+        throw runtime_error("popen() failed!");
+    }
+
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+
+    // Usunięcie nowej linii na końcu
+    if (!result.empty() && result.back() == '\n') {
+        result.pop_back();
+    }
+
+    // Pobranie pierwszej ścieżki
+    size_t pos = result.find('\n');
+    if (pos != string::npos) {
+        result = result.substr(0, pos);
+    }
+    return result;
+}
+
+int main() {
+    try {
+        string python_path = get_first_python_path();
+        string spolka;
+        cout << "Podaj nazwę spółki: ";
+        cin >> spolka;
+
+        // Konstruowanie komendy do uruchomienia skryptu Python
+        string command = python_path + " D:\\GitHub\\Projekt_MS_Programowanie2\\Wszystkie_kody\\scrapowanie.py " + spolka;
+
+        // Uruchomienie skryptu Python z nazwą spółki jako argumentem
+        int result = system(command.c_str());
+
+        if (result != 0) {
+            cerr << "Wystąpił błąd podczas uruchamiania skryptu Python" << endl;
+        }
+    } catch (const exception& ex) {
+        cerr << "Wystąpił wyjątek: " << ex.what() << endl;
+    }
+
+    return 0;
+}
